78b5bf7cbded63d648904d27711bb854addb8c42
IHaveNoNameIdea <arturpaal@gmail.com>
2024-02-17 16:51:31 +0200

    Update assignment.js

--- demos/assignment.js
+++ demos/assignment.js
@@ -72,7 +72,7 @@ function draw(timems) {
     let time = timems / 400;
 
     mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 50.0);
-    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(2, 2, 0), vec3.fromValues(0, 1, 0));
+    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
 
     mat4.fromXRotation(rotateXMatrix, time * 0.2036);
