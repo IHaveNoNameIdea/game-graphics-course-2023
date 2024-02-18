daa360f6b8564b42cb58ad0ba53cf4191b09ed5a
IHaveNoNameIdea <arturpaal@gmail.com>
2024-02-19 00:19:09 +0200

    Update assignment.js

--- demos/assignment.js
+++ demos/assignment.js
@@ -118,8 +118,8 @@ let shadowVertexShader = `
     }
 `;
 
-let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
-let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
+//let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
+//let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
 
 app.enable(PicoGL.DEPTH_TEST)
    .enable(PicoGL.CULL_FACE)
