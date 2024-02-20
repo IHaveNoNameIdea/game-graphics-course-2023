b612a4d95794e937136fd23393c157414221e514
IHaveNoNameIdea <arturpaal@gmail.com>
2024-02-20 14:31:28 +0200

    Update assignment.js

--- demos/assignment.js
+++ demos/assignment.js
@@ -126,7 +126,7 @@ let skyboxDrawCall = app.createDrawCall(skyboxProgram, skyboxArray)
 function draw(timems) {
     const time = timems * 0.001;
 
-    mat4.perspective(projMatrix, Math.PI / 5, app.width / app.height, 0.1, 100.0);
+    mat4.perspective(projMatrix, Math.PI / 7, app.width / app.height, 0.1, 100.0);
     let camPos = vec3.rotateY(vec3.create(), vec3.fromValues(0, 0.5, 2), vec3.fromValues(0, 0, 0), time * 0.05);
     mat4.lookAt(viewMatrix, camPos, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
